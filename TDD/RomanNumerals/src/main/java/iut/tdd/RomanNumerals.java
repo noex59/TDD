2e278735ce422b9baad1954c74e6b8260a8e0f91
vantourf <fvantours@gmail.com>
2016-04-22 09:07:26 +0200

    1er commit

---
+++ TDD/RomanNumerals/src/main/java/iut/tdd/RomanNumerals.java
@@ -0,0 +1,11 @@
+package iut.tdd;
+
+public class RomanNumerals {
+	public String convertToRoman(String arabe) {
+		return "I";
+	}
+
+	public String convertFromRoman(String roman) {
+		return "1";
+	}
+}
